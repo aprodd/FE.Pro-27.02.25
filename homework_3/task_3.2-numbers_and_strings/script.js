ae5bfab99e52db726b175247278018de188aba26
Oleg <alegprodd@gmail.com>
2025-03-08 15:01:19 +0200

    init hw_3

---
+++ homework_3/task_3.2-numbers_and_strings/script.js
@@ -0,0 +1,5 @@
+let myName = prompt("Enter your name");
+let myAge = prompt("How old are you?");
+let city = prompt("Your location?");
+
+console.log(`Hello, ${myName}. You are in ${city}. You are ${myAge} years old.`);
