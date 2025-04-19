c74a14a955642f2517c46551187a1f99285396b4
Oleg <alegprodd@gmail.com>
2025-04-19 21:28:25 +0300

    add hw_11.1

--- homework_11/task_11.1-pythagoras_table/script.js
+++ homework_11/task_11.1-pythagoras_table/script.js
@@ -1,1 +1,17 @@
+let table = document.createElement('table');
 
+document.body.append(table);
+table.style.border = '3px solid red';
+
+for (let i = 1; i <= 10; i++) {
+  let tr = document.createElement('tr');
+  table.appendChild(tr);
+  
+  for (let j = 1; j <= 10; j++) {
+    let td = document.createElement('td');
+    td.style.border = '1px solid black';
+    
+    tr.appendChild(td);
+    td.innerText = i * j;
+  }
+}
