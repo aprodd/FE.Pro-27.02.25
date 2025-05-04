b23c5f83da7f319055a1dbe4e40007496c158afd
Oleg <alegprodd@gmail.com>
2025-05-04 12:41:41 +0300

    add hw_17.3

--- homework_17/task_17.3-class_bank_account/script.js
+++ homework_17/task_17.3-class_bank_account/script.js
@@ -16,13 +16,10 @@ class BankAccount {
 }
 
 const account1 = new BankAccount(1000);
-
 console.log(account1.getBalance());
 
 account1.deposit(500);
-
 console.log(account1.getBalance());
 
 account1.withdraw(200);
-
 console.log(account1.getBalance());
