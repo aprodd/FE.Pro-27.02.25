305a0c65c6f5f111f585a2b64f4528c3205a53ec
Oleg <alegprodd@gmail.com>
2025-05-04 12:40:48 +0300

    init hw_17

---
+++ homework_17/task_17.3-class_bank_account/script.js
@@ -0,0 +1,28 @@
+class BankAccount {
+    constructor (sum) {
+        this.sum = sum;
+    }
+    getBalance() {
+        return this.sum;
+    }
+    deposit(amount) {
+        this.sum = this.sum + amount;
+        return this.sum;
+    }
+    withdraw(amount) {
+        this.sum = this.sum - amount;
+        return this.sum;
+    }
+}
+
+const account1 = new BankAccount(1000);
+
+console.log(account1.getBalance());
+
+account1.deposit(500);
+
+console.log(account1.getBalance());
+
+account1.withdraw(200);
+
+console.log(account1.getBalance());
